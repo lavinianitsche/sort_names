0a60c728935236bc568706a83550c3455a7c1ae0
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-07-07 08:09:30 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// codigo incompleto
-
 let nomes = [];
 
 function adicionarNome() {
@@ -19,77 +17,35 @@ function adicionarNome() {
     
     nomes.push(name);
     console.log(nomes);
-    alert("nome cadastrado com sucesso");
     console.log(`nome: ${name}`);
     nome.value = "";
+    
+    let participantes = "";
+    participantes = document.getElementById('listaDeParticipantes');
+    participantes.innerHTML += `<li>${name}</li>`;
 
-    // adicionar na lista de modo correto
-    
-    
-    
-    
-    console.log("Chegou onde eu queria");
-    
-    let participantes = document.getElementById('listaDeParticipantes');
-    
-    console.log(participantes);
-    // criar li e adicionar nomes
-    
-    participantes.value = "";
-    // 5. Criar um laço de repetição (for) para percorrer todos os elementos do array 'listaNomes':
-    // -> A cada repetição, concatenar uma nova tag <li> contendo o nome atual dentro da lista HTML
+    for (let i = 0; i < nomes.length; i++) {
+        name = nomes[i];
 
+    }
+
+    console.log(nomes);
+    
 }
 
 function sortearNome() {
 
-    if (nomes.length > 0) {
-        alert("teste");
+    let resultado;
+    resultado = nomes.splice(Math.floor(Math.random() * nomes.length), 1)[0];
+    
+
+    if (nomes.length < 0) {
+        alert("lista vazia");
+        return;
+
     } else {
-        alert("teste 2");
+        document.getElementById('txtSorteado').innerHTML = "resultado: " + resultado + " 🎉";
     }
 
 
-
-    let nome = document.getElementById("inputNome").value;
-    let resultado;
-    resultado = nomes.splice(Math.floor(Math.random() * nomes.length), 1)[0];
-
 }
-
-// function lista() {
-//     const lista = document.getElementById("listaDeParticipantes");
-//     lista.innerHTML = "";
-
-//     if (nomes.length === 0) {
-//         lista.innerHTML = '<div style="text-align: center; color: #999;">Nenhum ingrediente cadastrado</div>';
-//         return;
-//     }
-    
-//     nomes.forEach((nome, indice) => {
-//         const item = document.createElement('div');
-//         item.innerHTML = `<span>${indice}. ${nome}</span>`;
-//         lista.appendChild(item);
-//     })
-
-// }
-
-// function sortear() {
-
-// let texto = document.getElementById("entrada").value;
-// let tipo = document.getElementById("tipo").value;
-// let resultado;
-
-// if (tipo == "nomes") {
-
-//     let nomes = texto.split(",");
-//     resultado = nomes[Math.floor(Math.random() * nomes.length)];
-
-// } else {
-
-//     let n = texto.split("-");
-//     resultado = Math.floor(Math.random() * (n[1] - n[0] + 1)) + Number(n[0]);
-// }
-
-// document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
-// }
